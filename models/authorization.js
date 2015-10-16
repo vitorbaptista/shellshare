a864ea0192c09448660f0f844beea41c2c6447b1
Vitor Baptista <vitor@vitorbaptista.com>
2015-10-16 18:40:44 +0100

    Remove authorizations after 1 day
    This makes sure that we don't pollute the rooms' namespace. It could be used
    later to clean the rooms' collections as well, removing the data from rooms
    that aren't in use anymore.

--- models/authorization.js
+++ models/authorization.js
@@ -1,15 +1,33 @@
 var db = require('../db');
 var config = require('../config');
 
-var _collection = db.get().collection('authorizations');
+var _collection_cache = null;
+
+function _collection() {
+  if (_collection_cache == null) {
+    _collection_cache = db.get().collection('authorizations');
+    _collection_cache.ensureIndex('updatedAt', { expireAfterSeconds: 86400 });
+  }
+  return _collection_cache;
+}
 
 function isAuthorized(room, secret, callback) {
-  _collection.findOne({"_id": room}, function(err, item) {
-    if (!item) {
-      _collection.insertOne({"_id": room, "secret": secret});
+  _collection().findOne({'_id': room}, function(err, item) {
+    if (!item || item.secret == secret) {
+      _collection().updateOne({
+        '_id': room,
+      },
+      {
+        '_id': room,
+        'secret': secret,
+        'updatedAt': new Date(),
+      },
+      {
+        upsert: true,
+      });
       callback(true);
     } else {
-      callback(item.secret == secret);
+      callback(false);
     }
   });
 }
