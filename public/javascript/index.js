77aabffab238eda0ee43a36c5beb4067c991db7f
Vitor Baptista <vitor@vitorbaptista.com>
2015-10-21 18:30:11 +0100

    [#24] Toggle instructions between Linux/Mac

---
+++ public/javascript/index.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  var macosElement = document.getElementById('os-macos');
+  var linuxElement = document.getElementById('os-linux');
+
+  function toggleOS() {
+    if (linuxElement.checked) {
+      document.body.className = 'instructions-linux';
+    } else {
+      document.body.className = 'instructions-macos';
+    }
+  }
+
+  macosElement.onclick = toggleOS;
+  linuxElement.onclick = toggleOS;
+  toggleOS();
+})();
