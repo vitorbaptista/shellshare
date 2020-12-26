806014780487af12fe036316d45ff5414349d18e
Vitor Baptista <vitor@vitorbaptista.com>
2020-12-26 13:49:32 -0300

    Fix deprecation warning

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ var state = {
 exports.connect = function(url, done) {
   if (state.db) return done();
 
-  MongoClient.connect(url, function(err, db) {
+  MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
     if (err) return done(err);
     state.db = db;
     done();
