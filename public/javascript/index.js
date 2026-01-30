1fbf357be88f6eda0b2c346adacae1ac2b20d2a5
Vitor Baptista <vitor@vitorbaptista.com>
2026-01-30 06:46:41 -0300

    Adds Windows commands and docs on `shellshare server`

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -3,10 +3,13 @@
 
   var macosElement = document.getElementById('os-macos');
   var linuxElement = document.getElementById('os-linux');
+  var windowsElement = document.getElementById('os-windows');
 
   function toggleOS() {
     if (linuxElement.checked) {
       document.body.className = 'instructions-linux';
+    } else if (windowsElement.checked) {
+      document.body.className = 'instructions-windows';
     } else {
       document.body.className = 'instructions-macos';
     }
@@ -14,6 +17,18 @@
 
   macosElement.onclick = toggleOS;
   linuxElement.onclick = toggleOS;
+  windowsElement.onclick = toggleOS;
+
+  // Auto-detect OS
+  var platform = navigator.platform || '';
+  var userAgent = navigator.userAgent || '';
+
+  if (platform.indexOf('Win') !== -1 || userAgent.indexOf('Windows') !== -1) {
+    windowsElement.checked = true;
+  } else if (platform.indexOf('Linux') !== -1) {
+    linuxElement.checked = true;
+  }
+  // macOS is already checked by default in HTML
 
   toggleOS();
 })();
